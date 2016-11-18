45601dc930733bb410c54449fe3f449d04a58af9
Daniel D. ZHANG <zhangdanyangg@gmail.com>
2016-11-17 17:09:16 -0800

    update

---
+++ SumofOddElements.hs
@@ -0,0 +1,6 @@
+f arr = sum $ filter odd arr 
+
+-- This part handles the Input/Output and can be used as it is. Do not change or modify it.
+main = do
+   inputdata <- getContents
+   putStrLn $ show $ f $ map (read :: String -> Int) $ lines inputdata
