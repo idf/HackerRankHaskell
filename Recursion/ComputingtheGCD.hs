1a97c4c6cc2544219dbb6d4b60bc0bb7e733cfbd
Daniel D. ZHANG <zhangdanyangg@gmail.com>
2016-11-23 17:21:09 -0800

    hierarchy structure

---
+++ Recursion/ComputingtheGCD.hs
@@ -0,0 +1,14 @@
+module Main where
+
+gcd' :: Integral a => a -> a -> a
+gcd' n 0 = n
+gcd' n m = gcd' m $ mod n m
+
+-- This part is related to the Input/Output and can be used as it is
+-- Do not modify it
+main = do
+  input <- getLine
+  print . uncurry gcd' . listToTuple . convertToInt . words $ input
+ where
+  listToTuple (x:xs:_) = (x,xs)
+  convertToInt = map (read :: String -> Int)
